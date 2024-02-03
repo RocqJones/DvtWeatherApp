ee709d12c47c4b98b535ab64c5efc62e97a4730f
RocqJones <jonesmbindyo@gmail.com>
2024-02-03 18:01:40 +0300

    dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,15 +50,28 @@ android {
 }
 
 dependencies {
+    // variables
+    val retrofitVersion = "2.9.0"
+    val materialVersion = "1.11.0"
+    val material3Version = "1.1.2"
+    val coroutinesCore = "1.7.3"
+    val coroutinesAndroid = "1.7.1"
+    val lifecycleVersion = "2.7.0"
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+
+    // Compose UI
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+
+    // Material
+    implementation("com.google.android.material:material:$materialVersion")
+    implementation("androidx.compose.material3:material3:$material3Version")
+
+    // Test & Debug
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -66,4 +79,19 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+
+    // Lifecycle components: kt
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
+    // Lifecycle utilities: Compose
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesCore")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesAndroid")
 }
